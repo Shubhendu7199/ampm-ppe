cf8c6baa9715e33d421ed71d10b7816170633500
ZarakiKenpachi7 <shubhendu.upadhyay1@wpp.com>
2023-12-22 18:56:22 +0530

    Introducing Storage account module

--- law.tf
+++ law.tf
@@ -1,5 +1,5 @@
 resource "azurerm_log_analytics_workspace" "law" {
-  name                = "law-${var.opco}-01"
+  name                = "law-${var.opco}-${var.client_name}-01"
   location            = azurerm_resource_group.ampm.location
   resource_group_name = azurerm_resource_group.ampm.name
   sku                 = "PerGB2018"
