5a5580f56947b653c055883db6c51f404a4cb034
ZarakiKenpachi7 <shubhendu.upadhyay1@wpp.com>
2023-12-22 15:10:14 +0530

    Added network_watcher resources

--- modules/network/nsg.tf
+++ modules/network/nsg.tf
@@ -7,14 +7,36 @@ resource "azurerm_network_security_group" "nsg" {
   tags                = var.tags
 }
 
+resource "azurerm_resource_group" "net_watcher" {
+  name     = "network_watcher"
+  location = "southeastasia"
+}
+
+resource "azurerm_network_watcher" "ampm" {
+  name                = "wpp-wt-ampm-ppe-01"
+  location            = azurerm_resource_group.net_watcher.location
+  resource_group_name = azurerm_resource_group.net_watcher.name
+}
+
+resource "azurerm_storage_account" "test" {
+  name                = "sanetworklogswtampm01"
+  resource_group_name = azurerm_resource_group.net_watcher.name
+  location            = azurerm_resource_group.net_watcher.location
+
+  account_tier              = "Standard"
+  account_kind              = "StorageV2"
+  account_replication_type  = "LRS"
+  enable_https_traffic_only = true
+}
+
 resource "azurerm_network_watcher_flow_log" "nsg" {
   for_each                  = { for k, v in local.subnets_by_vnet : k => v if length(regexall(".*GatewaySubnet*", k)) == 0 }
   name                      = "nsg-${local.prefix}-${each.value.subnet_name}-logs"
-  network_watcher_name      = data.azurerm_network_watcher.default.name
-  resource_group_name       = data.azurerm_resource_group.network_watcher.name
+  network_watcher_name      = azurerm_network_watcher.ampm.name
+  resource_group_name       = azurerm_resource_group.net_watcher.name
   location                  = var.location
   network_security_group_id = azurerm_network_security_group.nsg[each.key].id
-  storage_account_id        = data.azurerm_storage_account.firewall.id
+  storage_account_id        = azurerm_storage_account.test.id
   enabled                   = true
   version                   = 2
 
