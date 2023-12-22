1cd309f9e5e278d492a041826e052e297354923d
ZarakiKenpachi7 <shubhendu.upadhyay1@wpp.com>
2023-12-22 17:23:42 +0530

    Added network_watcher resources

--- law.tf
+++ law.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "example" {
-  name     = "law-ampm-01"
+  name     = "rg-law-${var.opco}-01"
   location = "southeastasia"
 }
 
 resource "azurerm_log_analytics_workspace" "law" {
-  name                = "ampm-ppe-01"
+  name                = "law-${var.opco}-01"
   location            = "southeastasia"
   resource_group_name = azurerm_resource_group.example.name
   sku                 = "PerGB2018"
