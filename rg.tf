0b6be20ba976c3948a6061dbfaaf8788e0c9e882
ZarakiKenpachi7 <shubhendu.upadhyay1@wpp.com>
2023-12-22 16:34:55 +0530

    Added network_watcher resources

--- rg.tf
+++ rg.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "network" {
   name     = "rg-wpp-wt-${var.opco}-${module.location-lookup.location-lookup["location_short"]}-01"
-  location = var.location
+  location = var.region
 }
