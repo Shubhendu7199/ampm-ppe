e898ecd284cd1283a354768519ef25df59a70017
ZarakiKenpachi7 <shubhendu.upadhyay1@wpp.com>
2023-12-27 14:57:08 +0530

    Added SQL Flexible server

--- modules/flexible_sql_server/locals.tf
+++ modules/flexible_sql_server/locals.tf
@@ -1,25 +1,24 @@
 locals {
-  config = {
-    for key, server in azurerm_mysql_flexible_server.flexsqlserver : key => {
-      for config_key, config in var.sql_server[key].sql_configurations : config_key => {
+  config = flatten([
+    for key, server in azurerm_mysql_flexible_server.flexsqlserver : [
+      for config_key, config in var.sql_server[key].sql_configurations : {
         server_name         = server.name
         name                = config.name
         value               = config.value
         resource_group_name = server.resource_group_name
       }
-    }
-  }
+    ]
+  ])
 
-  fw_rules = {
-    for key, server in azurerm_mysql_flexible_server.flexsqlserver : key => {
-      for rule_key, rule in var.sql_server[key].sql_firewall_rules : rule_key => {
+  fw_rules = flatten([
+    for key, server in azurerm_mysql_flexible_server.flexsqlserver : [
+      for rule_key, rule in var.sql_server[key].sql_firewall_rules : {
         server_name         = server.name
         name                = rule.name
         start_ip_address    = rule.start_ip_address
         end_ip_address      = rule.end_ip_address
         resource_group_name = server.resource_group_name
       }
-    }
-  }
-
+    ]
+  ])
 }
