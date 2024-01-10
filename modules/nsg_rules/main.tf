699065a2c1686e9add09b411f41601da9b8ab564
ZarakiKenpachi7 <shubhendu.upadhyay1@wpp.com>
2024-01-10 07:48:38 +0530

    NSG Rules

--- modules/nsg_rules/main.tf
+++ modules/nsg_rules/main.tf
@@ -10,5 +10,5 @@ resource "azurerm_network_security_rule" "example" {
   source_address_prefix       = each.value.rule.source_address_prefix
   destination_address_prefix  = each.value.rule.destination_address_prefix
   resource_group_name         = var.resource_group_name
-  network_security_group_name = data.azurerm_network_security_group[each.value.nsg_name].example.name
+  network_security_group_name = data.azurerm_network_security_group.example[each.value.nsg_name].name
 }
